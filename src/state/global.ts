db4377e038d8c10e9185377f01e6c0d7896fb4b0
Zongguang Zhang <zzg19931223@outlook.com>
2021-04-13 19:46:51 +0800

    move some state from App to gloal state.

--- src/state/global.ts
+++ src/state/global.ts
@@ -1,6 +1,17 @@
+import {PaletteType} from "@material-ui/core"
 import {atom} from "recoil"
 
 export var currentPage = atom({
 	key: "currentPage",
 	default: ""
+})
+
+export var isDrawerOpenAtom = atom<boolean>({
+	key: "isDrawerOpenAtom",
+	default: false
+})
+
+export var themeAtom = atom<PaletteType>({
+	key: "themeAtom",
+	default: "light"
 })
